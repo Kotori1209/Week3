7dee6b393ee58dc60f4f3dddb3ecd7e3b798a6a9
Jacob Brookhouse <jacob_brookhouse76@yahoo.com>
2020-09-07 17:42:42 -0400

    Added About this program, and sample code

--- Jacob Solution/Program.cs
+++ Jacob Solution/Program.cs
@@ -1,4 +1,24 @@
-﻿
+﻿#region ABout this program
+
+/*Group
+ * Group Number: Group #1
+ * Group Members: Jacob B, Charles B, Tyler D, Bozhou C, Cyrus C
+ */
+
+
+/*Info
+ * Programmer: Jacob Brookhouse
+ * Date: 09/07/2020
+ * Purpose: Console app that calculates granola bar sales for the computer club.
+ *          Calculates how many are sold and sold for, percentage of sales to 
+ *          government association, and total profit. Let's user enter 
+ *          ammount sold and for how much.
+ *          
+ * Class: CITP 180
+ * Assignment: Chapter 3 - Programming Excercise 8
+ */
+
+#endregion
 
 using System;
 using System.Collections.Generic;
@@ -13,6 +33,9 @@ namespace Jacob_Solution
     {
         static void Main(string[] args)
         {
+            //Initial Code
+            Write("Hello World");
+            Read();
         }
     }
 }
